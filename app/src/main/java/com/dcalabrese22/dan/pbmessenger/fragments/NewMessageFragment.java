7abc7be945d16bb841a3806b131f5c5adba2a84b
dcalabrese22 <dcalabrese22@gmail.com>
2017-09-27 22:04:30 -0400

    feat: added autocomplete values to new message to

--- app/src/main/java/com/dcalabrese22/dan/pbmessenger/fragments/NewMessageFragment.java
+++ app/src/main/java/com/dcalabrese22/dan/pbmessenger/fragments/NewMessageFragment.java
@@ -7,11 +7,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
+import com.dcalabrese22.dan.pbmessenger.Objects.PbConversation;
 import com.dcalabrese22.dan.pbmessenger.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class NewMessageFragment extends Fragment {
+
+    private AutoCompleteTextView mName;
 
 
     public NewMessageFragment() {
@@ -24,6 +40,37 @@ public class NewMessageFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_new_message, container, false);
+        mName = (AutoCompleteTextView) rootView.findViewById(R.id.et_new_message_to);
+
+        final List<String> autoCompleteNames = new ArrayList<>();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("conversations");
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        final String userId = user.getUid();
+        Query userNameQuery = reference.child(userId);
+        userNameQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    PbConversation conversation = snapshot.getValue(PbConversation.class);
+                    String userName = conversation.getUser();
+                    autoCompleteNames.add(userName);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
+                android.R.layout.simple_dropdown_item_1line, autoCompleteNames);
+        mName.setAdapter(adapter);
+
+
+
+
         return rootView;
     }
 
