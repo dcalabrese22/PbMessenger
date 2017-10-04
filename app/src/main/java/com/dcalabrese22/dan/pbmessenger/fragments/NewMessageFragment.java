3b3b1f9091082eac233274613476fcba6dae19f7
dcalabrese22 <dcalabrese22@gmail.com>
2017-10-04 15:38:39 -0400

    feat: updated ui styling for incoming/outgoing messages and new message fragment

--- app/src/main/java/com/dcalabrese22/dan/pbmessenger/fragments/NewMessageFragment.java
+++ app/src/main/java/com/dcalabrese22/dan/pbmessenger/fragments/NewMessageFragment.java
@@ -3,12 +3,15 @@ package com.dcalabrese22.dan.pbmessenger.fragments;
 
 import android.os.Bundle;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.dcalabrese22.dan.pbmessenger.Objects.PbConversation;
 import com.dcalabrese22.dan.pbmessenger.R;
@@ -41,12 +44,34 @@ public class NewMessageFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_new_message, container, false);
         mName = (AutoCompleteTextView) rootView.findViewById(R.id.et_new_message_to);
+        final EditText subject = (EditText) rootView.findViewById(R.id.et_new_message_subject);
+        final EditText body = (EditText) rootView.findViewById(R.id.et_new_message_body);
+        FloatingActionButton fab = (FloatingActionButton) rootView.findViewById(R.id.fab_new_message);
+
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("conversations");
+
+        DatabaseReference uidReference = reference.child(userId);
+        DatabaseReference messages = reference.child("messages");
+
+        fab.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (mName.getText().toString().equals("") || subject.getText().toString().equals("")
+                        || body.getText().toString().equals("")) {
+                    Toast.makeText(getContext(), R.string.missing_fields, Toast.LENGTH_SHORT).show();
+                } else {
+
+                }
+            }
+        });
 
         final List<String> autoCompleteNames = new ArrayList<>();
 
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("conversations");
+        reference = FirebaseDatabase.getInstance().getReference("conversations");
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        final String userId = user.getUid();
+        
         Query userNameQuery = reference.child(userId);
         userNameQuery.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
